7f0eb72460b27914198ae51fdf887a75330ce3ae
RosVor <ros.vorokh@gmail.com>
2023-07-11 21:10:15 +0300

    add flatpickr html and notiflix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,6 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import Notiflix from "notiflix";
+import "notiflix/dist/notiflix-3.2.6.min.css";
 function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
