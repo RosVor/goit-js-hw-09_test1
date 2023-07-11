302a56312adddfd362840b00feacda9be589bdef
RosVor <ros.vorokh@gmail.com>
2023-07-11 20:49:50 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
-
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+  let intervalId;
+  function startColorSwitching() {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+  function stopColorSwitching() {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    clearInterval(intervalId);
+  }
+  startButton.addEventListener('click', startColorSwitching);
+  stopButton.addEventListener('click', stopColorSwitching);
