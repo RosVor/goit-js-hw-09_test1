b69930103ad36edb7a241eecba910361dd497c4c
RosVor <ros.vorokh@gmail.com>
2023-07-12 22:52:03 +0300

    deb

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -14,9 +14,9 @@ form.addEventListener('submit', function(event) {
 
   for (let i = 1; i <= amount; i++) {
     createPromise(i, delay).then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
     }).catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
     });
   }
 });
