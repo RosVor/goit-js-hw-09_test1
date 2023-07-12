6b23235f1a16b7a3918ab26a62f8108ab536b650
RosVor <ros.vorokh@gmail.com>
2023-07-12 21:22:07 +0300

    task 3?

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
+import { Notify } from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const delayInput = this.elements.delay;
+  const stepInput = this.elements.step;
+  const amountInput = this.elements.amount;
+  const delay = Number(delayInput.value);
+  const step = Number(stepInput.value);
+  const amount = Number(amountInput.value);
+  createPromises(delay, step, amount)
+    .then((results) => {
+      Notify.success('All promises resolved');
+      console.log('All promises resolved:', results);
+    })
+    .catch((error) => {
+      Notify.failure('Error occurred');
+      console.log('Error occurred:', error);
+    });
+});
+
+  function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+async function createPromises(initialDelay, step, amount) {
+  const promises = [];
+  for (let i = 0; i < amount; i++) {
+    const delay = initialDelay + step * i;
+    const promise = createPromise(i + 1, delay);
+    promises.push(promise);
   }
+  return Promise.allSettled(promises);
 }
